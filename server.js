73904f6ff23c86439f2a8cc54a819777b0fc5d0e
vianey.jean <vianey.jean@ymail.com>
2025-04-15 13:25:07 +0400

    mise a jour le port

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use((req, res) => {
 });
 
 // Lancement du serveur
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
